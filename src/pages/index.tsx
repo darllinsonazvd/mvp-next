de6c589c7590aa58aca0c8569435cc1b248e84f8
Darllinson Azevedo <azevedo.darllinson@gmail.com>
2022-08-03 00:52:40 -0300

    Finished login with GitHub

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,96 @@
-export default function Home() {
+import { signIn } from 'next-auth/react';
+
+import { GithubLogo } from 'phosphor-react';
+
+export default function SignIn() {
+  function handleSignIn() {
+    signIn('github');
+  }
+
   return (
-    <div>
-      <h1>Hello World!</h1>
-    </div>
+    <>
+      <div className="h-screen bg-gray-50 flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+        <div className="max-w-md w-full space-y-8">
+          <div>
+            <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+              Faça login na plataforma
+            </h2>
+          </div>
+          <form className="mt-8 space-y-6" action="#" method="POST">
+            <input type="hidden" name="remember" defaultValue="true" />
+            <div className="rounded-md shadow-sm -space-y-px">
+              <div>
+                <label htmlFor="email-address" className="sr-only">
+                  Email address
+                </label>
+                <input
+                  id="email-address"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  required
+                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-violet-500 focus:border-violet-500 focus:z-10 sm:text-sm"
+                  placeholder="Email"
+                />
+              </div>
+              <div>
+                <label htmlFor="password" className="sr-only">
+                  Password
+                </label>
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  required
+                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-violet-500 focus:border-violet-500 focus:z-10 sm:text-sm"
+                  placeholder="Senha"
+                />
+              </div>
+            </div>
+
+            <div className="flex items-center justify-between">
+              <div className="flex items-center">
+                <input
+                  id="remember-me"
+                  name="remember-me"
+                  type="checkbox"
+                  className="h-4 w-4 text-violet-600 focus:ring-violet-500 border-gray-300 rounded"
+                />
+                <label htmlFor="remember-me" className="ml-2 block text-sm text-gray-900">
+                  Manter conectado
+                </label>
+              </div>
+
+              <div className="text-sm">
+                <a href="#" className="font-medium text-violet-600 hover:text-violet-500">
+                  Esqueceu sua senha?
+                </a>
+              </div>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-violet-600 hover:bg-violet-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-violet-500"
+              >
+                Entrar
+              </button>
+            </div>
+            <div id="divider" className="w-full h-px bg-gray-300"></div>
+            <div>
+              <button
+                type="submit"
+                className="group relative w-full flex justify-center py-2 px-4 border-2 border-violet-600 text-sm font-medium rounded-md text-violet-600 bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-violet-500"
+                onClick={handleSignIn}
+              >
+                <GithubLogo className="mr-2" size={20} />
+                Conectar com GitHub
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
   );
 }
